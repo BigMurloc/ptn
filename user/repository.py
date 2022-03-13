a087f9746ad2c0f7bed882ca69720882fdf6f5d2
BigMurloc <s21159@pjwstk.edu.pl>
2022-03-13 13:25:11 +0100

    add headers if file not exists yet while saving user

--- user/repository.py
+++ user/repository.py
@@ -1,10 +1,21 @@
 import csv
+from os.path import exists
 
 DB_PATH = 'resources/db.csv'
 
 
-def save(*data):
+def save(username, hashed_password):
+    should_write_headers = False
+    if not exists(DB_PATH):
+        should_write_headers = True
+
     with open(DB_PATH, 'a') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([*data])
+        fieldnames = ['userna'
+                      'me', 'password']
+        writer = csv.DictWriter(csvfile, fieldnames)
+
+        if should_write_headers:
+            writer.writeheader()
+
+        writer.writerow({'username': username, 'password': hashed_password})
         csvfile.close()
