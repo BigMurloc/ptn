a081f5f54e345b2127116919ba5fc33bf278b33d
BigMurloc <s21159@pjwstk.edu.pl>
2022-03-13 13:54:37 +0100

    repeat asking user when registering on input failure

--- user/register.py
+++ user/register.py
@@ -11,21 +11,46 @@ INVALID_CHARACTERS_REGEX = "^(?=.*\\s)"
 
 # todo loop user over and over if password does not match requirements
 def register_user():
+    username = get_username()
+    password = get_password()
+
+    save(username, hash_password(password.encode('utf8')))
+
+
+def get_username():
     username = input("Enter username: ")
+    while verify_username(username) is not True:
+        username = input("Enter username: ")
+    return username
+
+
+def get_password():
     password = getpass()
-    if verify_password(password) and verify_username(username):
-        save(username, hash_password(password.encode('utf8')))
+    while verify_password(password) is not True:
+        password = getpass()
+    return password
 
 
 def verify_username(username):
-    return re.search(INVALID_CHARACTERS_REGEX, username) is None
+    if re.search(INVALID_CHARACTERS_REGEX, username) is not None:
+        print("Username contains invalid characters, please try again")
+        return False
+
+    return True
 
 
 def verify_password(password):
     if re.search(INVALID_CHARACTERS_REGEX, password) is not None:
+        print("Password contains invalid characters like whitespace, please try again")
         return False
 
-    return re.search(STRONG_PASSWORD_REGEX, password) is not None
+    if re.search(STRONG_PASSWORD_REGEX, password) is None:
+        print("Password is not strong enough, "
+              "password should be at least 8 characters long and include one of each: "
+              "cipher, upper letter, lower letter ")
+        return False
+
+    return True
 
 
 def hash_password(password):
