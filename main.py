cab22fabf66caa4516238dadd9d5738c8a95b3ce
BigMurloc <s21159@pjwstk.edu.pl>
2022-03-20 21:11:47 +0100

    todos

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from command import dispatch
 
 if __name__ == '__main__':
     dispatch()
-# - insensitive username
-# login registered user
-# delete user
+
+# todo insensitive username
+# todo delete user
+# todo filtering (escaping username with quoting marks)
